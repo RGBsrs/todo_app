b3a2f45b3b3ecfb1557c17d3c911235ade924d01
RGBSrs <grjanko.d.a@gmail.com>
2021-04-27 17:07:04 +0300

    add new field updated_at

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 from src import db
 
 
@@ -7,9 +8,18 @@ class Todo(db.Model):
     __tablename__ = 'todos'
 
     id = db.Column(db.Integer, primary_key = True)
+    uuid = db.Column(db.String(36), unique = True)
     title = db.Column(db.String(120), nullable = False)
     completed = db.Column(db.Boolean, default = False)
-    timestamp = db.Column(db.Date, default = datetime.datetime.now())  
+    created_at = db.Column(db.DateTime, default = datetime.datetime.now())
+    updated_at = db.Column(db.DateTime)  
+
+    def __init__(self, title, completed = False, updated_at = datetime.datetime.now()):
+        self.title = title
+        self.completed = completed
+        self.updated_at = updated_at
+        self.uuid = str(uuid.uuid4())
+
 
     def __repr__(self):
         return f'Todo({self.title},{self.completed}, timestamp = {self.timestamp})' 
--- src/services/todo_service.py
+++ src/services/todo_service.py
@@ -6,6 +6,6 @@ class TodoService:
     def fetch_all_todos(session):
         return session.query(Todo)
 
-    # @classmethod
-    # def fetch_film_by_uuid(cls, session, uuid):
-    #     return cls.fetch_all_films(session).filter_by(uuid = uuid).first()
+    @classmethod
+    def fetch_todo_by_uuid(cls, session, uuid):
+        return cls.fetch_all_todos(session).filter_by(uuid = uuid).first()
