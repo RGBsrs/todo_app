26182ac02193d56626f7dab8d4bbbf4211068f03
RGBSrs <grjanko.d.a@gmail.com>
2021-04-29 12:38:45 +0300

    add logout api

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,6 @@
 from src import app
 from src.resources.todos import TodoListApi
-from src.resources.auth import LoginApi, RegisterApi
+from src.resources.auth import LoginApi, RegisterApi,LogoutApi
 
 
 todo_list_api = TodoListApi.as_view('todos')
@@ -11,6 +11,8 @@ app.add_url_rule('/api/v1/todos/<id>', view_func=todo_list_api, strict_slashes =
 login_api = LoginApi.as_view('login')
 app.add_url_rule('/api/v1/login', view_func=login_api, strict_slashes = False)
 
+logout_api = LogoutApi.as_view('logout')
+app.add_url_rule('/api/v1/logout', view_func=logout_api, strict_slashes = False)
 
 register_api = RegisterApi.as_view('register')
 app.add_url_rule('/api/v1/register', view_func=register_api, strict_slashes = False)
